9f40beea0d7bfbc5c63fe486ff4c55faa63c4e80
Jno <35944775+Jno1995@users.noreply.github.com>
2019-09-24 19:40:25 +0800

    Initial commit

---
+++ assets/scripts/ListViewCtrl.ts
@@ -0,0 +1,149 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+    @property(cc.Label)
+    itemCountLabel: cc.Label = null;
+
+    @property(cc.Prefab)
+    prefabCell: cc.Prefab = null;
+
+    @property(cc.Integer)
+    itemCount: number = 0;
+
+    @property(cc.Integer)
+    prefabCellHeight: number = 0;
+
+    @property(cc.Integer)
+    prefabCellWidth: number = 0;
+
+    @property(cc.ScrollView)
+    scrollView:cc.ScrollView = null;
+
+    @property(cc.Integer)
+    spacing: number = 0;
+
+    @property(cc.String)
+    cellScriptName: string = "";
+
+    @property(cc.Node)
+    content: cc.Node = null;
+
+    items: cc.Node[] = [];
+
+    updateTimer: number = 0;
+    
+    updateInterval: number = 0.1;
+    
+    lastContentPosX: number = 0;
+    
+    lastContentPosY: number = 0;
+    
+    _bufferZone: number = 0; //when item is away from bufferZone, we relocate it
+    _spawnCount: number = 0; // how many items we actually spawn
+    _totalCount: number = 0; // how many items we need for the whole list
+
+    onLoad () {
+        this._bufferZone = this.scrollView.node.height;
+        this.content.anchorX = 0.5;
+        this.content.anchorY = 1;
+        if (this.scrollView.horizontal) {
+            this._bufferZone = this.scrollView.node.width;
+            this.content.anchorX = 0;
+            this.content.anchorY = 0.5;
+        }
+    }
+
+    init () {
+        this.items = [];
+        this.lastContentPosY = 0;
+        this.lastContentPosX = 0;
+
+        if (this.scrollView.vertical && this.scrollView.horizontal) {
+            return;
+        }
+
+        this._totalCount = this.itemCount;
+
+        this._spawnCount = Math.floor(this._bufferZone / (this.prefabCellHeight + this.spacing)) + 4;
+        if (this.scrollView.horizontal) {
+            this._spawnCount = Math.floor(this._bufferZone / (this.prefabCellWidth + this.spacing)) + 4;
+        }
+
+        if (this._spawnCount > this._totalCount) {
+            this._spawnCount = this._totalCount;
+        }
+
+        if (this.scrollView.vertical) {
+            this.content.height = this._totalCount * (this.prefabCellHeight + this.spacing) + this.spacing; // get total content height
+        } else if (this.scrollView.horizontal) {
+            this.content.width = this._totalCount * (this.prefabCellWidth + this.spacing) + this.spacing; // get total content width
+        }
+        for (let i = 0; i < this._spawnCount; ++i) { // spawn items, we only need to do this once
+            let item = cc.instantiate(this.prefabCell);
+            this.content.addChild(item);
+
+            if (this.scrollView.vertical) {
+                item.setPosition(0, -item.height * (0.5 + i) - this.spacing * (i + 1));
+            } else if (this.scrollView.horizontal) {
+                item.setPosition(item.width * (i + 0.5) + this.spacing * (i + 1), 0);
+            }
+            this.items.push(item);
+        }
+    }
+
+    getPositionInView (item) { // get item position in scrollview's node space
+        let worldPos = item.parent.convertToWorldSpaceAR(item.position);
+        let viewPos = this.scrollView.node.convertToNodeSpaceAR(worldPos);
+        return viewPos;
+    }
+
+    update (dt) {
+        this.itemCountLabel.string = (this.content.childrenCount).toString();
+        
+        this.updateTimer += dt;
+        if (this.updateTimer < this.updateInterval) return; // we don't need to do the math every frame
+        this.updateTimer = 0;
+        let items = this.items;
+        let buffer = this._bufferZone;
+        if (this.scrollView.vertical) {
+            let isDown = this.scrollView.content.y < this.lastContentPosY; // scrolling direction
+            let offset = (this.prefabCellHeight + this.spacing) * items.length;
+            for (let i = 0; i < items.length; ++i) {
+                let viewPos = this.getPositionInView(items[i]);
+                if (isDown) {
+                    // if away from buffer zone and not reaching top of content
+                    if (viewPos.y < -buffer && items[i].y + offset < 0) {
+                        items[i].y = items[i].y + offset;
+                    }
+                } else {
+                    // if away from buffer zone and not reaching bottom of content
+                    if (viewPos.y > buffer / 2 + (this.prefabCellHeight + this.spacing) / 2 && items[i].y - offset > -this.content.height) {
+                        items[i].y = items[i].y - offset;
+                    }
+                }
+            }
+            // update lastContentPosY
+            this.lastContentPosY = this.scrollView.content.y;
+        } else if (this.scrollView.horizontal) {
+            let isRight = this.scrollView.content.x > this.lastContentPosX; // scrolling direction
+            let offset = (this.prefabCellWidth + this.spacing) * items.length;
+            for (let i = 0; i < items.length; ++i) {
+                let viewPos = this.getPositionInView(items[i]);
+                if (isRight) {
+                    // if away from buffer zone and not reaching top of content
+                    if (viewPos.x > buffer / 2 + (this.prefabCellWidth + this.spacing) / 2 && items[i].x - offset > 0) {
+                        items[i].x = items[i].x - offset;
+                    }
+                } else {
+                    // if away from buffer zone and not reaching bottom of content
+                    if (viewPos.x < -buffer / 2 - (this.prefabCellWidth + this.spacing) / 2 && items[i].x + offset < this.content.width) {
+                        items[i].x = items[i].x + offset;
+                    }
+                }
+            }
+            // update lastContentPosX
+            this.lastContentPosX = this.scrollView.content.x;
+        }
+    }
+};
---
+++ assets/scripts/Main.ts
@@ -0,0 +1,163 @@
+// Learn TypeScript:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(cc.ScrollView)
+    scrollview: cc.ScrollView = null;
+    
+    @property(cc.Label)
+    itemCountLabel: cc.Label = null;
+
+    @property(cc.Camera)
+    itemCamera: cc.Camera = null;
+
+    @property(cc.Prefab)
+    itemTemplet: cc.Prefab = null;
+
+    texture: cc.RenderTexture = null;
+
+    _canvas: HTMLCanvasElement = null;
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    onButton_0_Clicked () {
+        this.scrollview.content.destroyAllChildren();
+        this.scrollview.content.getComponent(cc.Layout).enabled = false;
+        this.scrollview.node.getComponent("ListViewCtrl").init();
+    }
+
+    /**
+	 * 分帧执行 Generator 逻辑
+	 *
+	 * @param generator 生成器
+	 * @param duration 持续时间（ms），每次执行 Generator 的操作时，最长可持续执行时长。假设值为8ms，那么表示1帧（总共16ms）下，分出8ms时间给此逻辑执行
+	 */
+
+	private executePreFrame(generator: Generator, duration: number) {
+		return new Promise((resolve, reject) => {
+			let gen = generator;
+			// 创建执行函数
+			let execute = () => {
+				// 执行之前，先记录开始时间
+				let startTime = new Date().getTime();
+
+				// 然后一直从 Generator 中获取已经拆分好的代码段出来执行
+				for (let iter = gen.next(); ; iter = gen.next()) {
+					// 判断是否已经执行完所有 Generator 的小代码段，如果是的话，那么就表示任务完成
+					if (iter == null || iter.done) {
+						resolve();
+						return;
+					}
+
+					// 每执行完一段小代码段，都检查一下是否已经超过我们分配的本帧，这些小代码端的最大可执行时间
+					if (new Date().getTime() - startTime > duration) {
+						// 如果超过了，那么本帧就不在执行，开定时器，让下一帧再执行
+						this.scheduleOnce(() => {
+							execute();
+						});
+						return;
+					}
+				}
+			};
+
+			// 运行执行函数
+			execute();
+		});
+	}
+
+	private *_getItemGenerator(itemNode: cc.Node, length: number) {
+        this.init(itemNode);
+		for (let i = 0; i < length; i++) {
+			yield this.renderTextureItem(itemNode, i);
+        }
+        itemNode.destroy();
+    }
+    
+    renderTextureItem (itemNode: cc.Node, i) {
+        itemNode.getChildByName("nickname_lbl").getComponent(cc.Label).string = "user_" + i;
+        this.createCanvas(itemNode);
+        var img = this.createImg();
+        this.addItem(img);
+    }
+
+    async onButton_1_Clicked () {
+        this.scrollview.content.destroyAllChildren();
+        this.scrollview.content.getComponent(cc.Layout).enabled = true;
+        let itemNode = cc.instantiate(this.itemTemplet);
+        itemNode.group = "Item";
+        this.node.addChild(itemNode);
+        await this.executePreFrame(this._getItemGenerator(itemNode, 500), 1);
+    }
+
+    init (item: cc.Node) {
+        let texture = new cc.RenderTexture();
+        let gl = cc.game._renderContext;
+        texture.initWithSize(item.width, item.height, gl.STENCIL_INDEX8);
+        this.itemCamera.targetTexture = texture;
+        this.texture = texture;
+    }
+
+    // create the img element
+    createImg () {
+        // return the type and dataUrl
+        var dataURL = this._canvas.toDataURL("image/png");
+        var img = document.createElement("img");
+        img.src = dataURL;
+        return img;
+    }
+
+    // create the canvas and context, filpY the image Data
+    createCanvas (visitNode: cc.Node) {
+        let width = this.texture.width;
+        let height = this.texture.height;
+        if (!this._canvas) {
+            this._canvas = document.createElement('canvas');
+
+            this._canvas.width = width;
+            this._canvas.height = height;
+        }
+        let ctx = this._canvas.getContext('2d');
+        this.itemCamera.render(visitNode);
+        let data = this.texture.readPixels();
+        // write the render data
+        let rowBytes = width * 4; 
+        for (let row = 0; row < height; row++) {
+            let srow = height - 1 - row;
+            let imageData = ctx.createImageData(width, 1);
+            let start = srow * width * 4;
+            for (let i = 0; i < rowBytes; i++) {
+                imageData.data[i] = data[start + i];
+            }
+
+            ctx.putImageData(imageData, 0, row);
+        }
+        return this._canvas;
+    }
+
+    // show on the canvas
+    addItem (img) {
+        let texture = new cc.Texture2D();
+        texture.initWithElement(img);
+
+        let spriteFrame = new cc.SpriteFrame();
+        spriteFrame.setTexture(texture);
+
+        let node = new cc.Node();
+        let sprite = node.addComponent(cc.Sprite);
+        sprite.spriteFrame = spriteFrame;
+
+        node.parent = this.scrollview.content;
+    }
+}
